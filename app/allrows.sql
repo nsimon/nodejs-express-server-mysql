84aa97f53b36589e19efff5542f1f04e261d50e4
Neil Simon <nsimon@solidcode.com>
2019-05-07 15:04:34 -0600

    initial checkin

---
+++ app/allrows.sql
@@ -0,0 +1,10 @@
+-- Date ... 05/07/2019
+-- Desc ... allrows.sql
+
+-- script to display database contents
+USE movieapp;
+
+-- display database data
+SELECT * FROM directors;
+SELECT * FROM movies;
+
