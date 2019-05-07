a6c1bd586a84e4b0741b20624e2d8def93f70c76
Neil Simon <nsimon@solidcode.com>
2019-05-07 15:04:51 -0600

    ported by mysql

--- app/db.getall.sh
+++ app/db.getall.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 
-# Date ... 04/23/2019
-# Desc ... db.getall.sh
+# Date ..... 04/23/2019
+# Module ... db.getall.sh
+# Desc ..... displays all rows in directors and movies
 
-# Gets all documents in all collections
-
-printf "db.getall.sh\n"
-printf "\n"
-
-printf "========================\n"
-printf "db.getCollectionNames ()\n"
-printf "========================\n"
-printf "\n"
-mongo movieapp --quiet --eval "db.getCollectionNames ()"
-printf "\n"
-
-printf "================\n"
-printf "db.users.find ()\n"
-printf "================\n"
-printf "\n"
-mongo movieapp --quiet --eval "db.users.find ()"
-printf "\n"
-
-printf "====================\n"
-printf "db.directors.find ()\n"
-printf "====================\n"
-printf "\n"
-mongo movieapp --quiet --eval "db.directors.find ()"
-printf "\n"
-
-printf "=================\n"
-printf "db.movies.find ()\n"
-printf "=================\n"
-printf "\n"
-mongo movieapp --quiet --eval "db.movies.find ()"
-printf "\n"
+mysql --user=nsimon --password='cwb206' --table < allrows.sql | tee allrows.log
 
